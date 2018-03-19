d6177550dc70aab23ec5fc1ea9bffecbe98a8d89
wangcl <wangcl@live.com>
2018-03-19 23:27:59 +0800

    add AdminBootApplication and its test Class

---
+++ src/main/java/me/wangcl/showcase/adminboot/AdminBootApplication.java
@@ -0,0 +1,12 @@
+package me.wangcl.showcase.adminboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminBootApplication.class, args);
+	}
+}
