90134d6ece683e30d230b90d3584ed41e84c230a
wangcl <wangcl@live.com>
2018-03-19 23:58:16 +0800

    AdminBootApplication extends SpringBootServletInitializer，使应用war支持部署至tomcat

--- src/main/java/me/wangcl/showcase/adminboot/AdminBootApplication.java
+++ src/main/java/me/wangcl/showcase/adminboot/AdminBootApplication.java
@@ -2,9 +2,16 @@ package me.wangcl.showcase.adminboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AdminBootApplication {
+public class AdminBootApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AdminBootApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminBootApplication.class, args);
