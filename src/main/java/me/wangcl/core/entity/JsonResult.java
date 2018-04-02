eaf0ff5fc0005d7101a116d17067b133cb1b213f
wangcl <wangcl@live.com>
2018-04-02 21:28:31 +0800

    增加无返回值的ajax服务调用的json结果对象

---
+++ src/main/java/me/wangcl/core/entity/JsonResult.java
@@ -0,0 +1,51 @@
+package me.wangcl.core.entity;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.io.Serializable;
+
+/**
+ * json格式的服务调用结果。
+ *
+ * @author wangcl
+ */
+public class JsonResult implements Serializable {
+	private static final long serialVersionUID = -3566716150016626693L;
+
+	private Integer status; // 调用结果，0：成功，非0：失败
+	private String message; // 结果显示信息，一般为成功提示或错误信息
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return EqualsBuilder.reflectionEquals(this, o);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+}
