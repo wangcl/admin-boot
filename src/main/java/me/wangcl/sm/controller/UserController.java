683f4f5c40e8fd8921d2ede6edb353c27e067b06
wangcl <wangcl@live.com>
2018-04-09 14:29:42 +0800

    UserController增加@PreAuthorize配置，防止未授权用户通过浏览器输入功能地址直接访问

--- src/main/java/me/wangcl/sm/controller/UserController.java
+++ src/main/java/me/wangcl/sm/controller/UserController.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -47,6 +48,7 @@ public class UserController {
 	 * 返回: {model}/index.jsp
 	 */
 	@RequestMapping(value = "", method = RequestMethod.GET)
+	@PreAuthorize("hasRole('ADMIN') or hasAnyAuthority('user')")
 	public String index() {
 		return "sys/user/index";
 	}
